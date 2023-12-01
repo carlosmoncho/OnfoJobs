d6f4c88a5f2589153468d830a450fd12fdb2f19c
carlosmoncho <carlosmonchomolla@gmail.com>
2023-12-01 11:41:58 +0100

    [ADD] crear vacantes

---
+++ app/Livewire/CrearVacante.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Categoria;
+use App\Models\Salario;
+use App\Models\Vacante;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
+
+class CrearVacante extends Component
+{
+    public $titulo;
+    public $salario;
+    public $categoria;
+    public $empresa;
+    public $ultimo_dia;
+    public $descripcion;
+    public $imagen;
+
+    use WithFileUploads;
+
+    protected $rules = [
+        'titulo' => 'required|string',
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required',
+        'ultimo_dia' => 'required',
+        'descripcion' => 'required',
+        'imagen' => 'required|image|max:1024',
+    ];
+
+    public function render()
+    {
+        $salarios = Salario::all();
+        $categorias = Categoria::all();
+        return view('livewire.crear-vacante', ['salarios' => $salarios,'categorias' => $categorias]);
+    }
+
+    public function crearVacante(){
+        $datos = $this->validate();
+
+        $imagen = $this->imagen->store('public/vacantes');
+        $datos['imagen'] = str_replace('public/vacantes','',$imagen); 
+
+        Vacante::create([
+            'titulo' => $datos['titulo'],
+            'salario_id' => $datos['salario'],
+            'categoria_id' => $datos['categoria'],
+            'empresa' => $datos['empresa'],
+            'ultimo_dia' => $datos['ultimo_dia'],
+            'descripcion' => $datos['descripcion'],
+            'imagen' => $datos['imagen'],
+            'user_id' => auth()->user()->id
+        ]);
+
+        session()->flash('mensaje', 'La vacante se publicó correctamente');
+
+        return redirect()->route('vacantes.index');
+    }
+}
